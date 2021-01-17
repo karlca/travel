2d97af51c2db37cc6f1f001e29576a54943e39e9
kirl <luokes@gmail.com>
2021-01-17 20:13:20 +0800

    first

---
+++ src/pages/home/home.vue
@@ -0,0 +1,15 @@
+import { template } from "babel-core";
+
+<template>
+    <div>Home</div>
+</template>
+
+<script>
+    export default {
+        name:'Home'
+        }
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,15 @@
+import { template } from "babel-core";
+
+<template>
+    <div>List</div>
+</template>
+
+<script>
+    export default {
+        name:'List'
+        }
+</script>
+
+<style>
+
+</style>
